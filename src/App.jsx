41f974bb749961725ec1f3dd9c2d30fc399f2cd4
Birgit <birgit.bartenschlager@web.de>
2023-08-30 12:57:05 +0200

    implement get meme image

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Form from "./components/Form";
 import Meme from "./components/Meme";
 
 function App() {
   return (
     <div className="general-container">
       <Header />
-      <Form />
       <Meme />
     </div>
   );
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import memeimg from "../assets/memeimg.png";
+import React, { useState } from "react";
+import data from "../../memesData.js";
 
 function Meme() {
+  const [memeImage, setMemeImage] = useState("");
+
+  function getMemeImage() {
+    const randomIndex = Math.floor(Math.random() * data.data.memes.length);
+    setMemeImage(data.data.memes[randomIndex].url);
+    console.log(memeImage);
+  }
+
   return (
-    <div className="meme-container">
-      <img src={memeimg} alt="meme" />
+    <div className="form-container">
+      <div className="form-input-container">
+        <input type="text" name="" id="" placeholder="Shut up" />
+        <input type="text" name="" id="" placeholder="and take my money" />
+      </div>
+      <button className="form-cta" onClick={getMemeImage}>
+        Get a new meme image
+      </button>
+      <img className="meme-image" src={memeImage} alt="meme" />
     </div>
   );
 }
